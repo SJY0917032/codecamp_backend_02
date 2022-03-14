9d28ef13d85b77d43a1894f4b09ef9be00ab72a4
SJY0917032 <sea71511@gmail.com>
2022-03-14 12:49:14 +0900

    연습문제

---
+++ algorithm/day01/example.js
@@ -0,0 +1,11 @@
+// function solution(max, min) {
+//   count = 0;
+
+//   for (i = min; i <= max; i = i + 2) {
+//     count++;
+//   }
+
+//   return count;
+// }
+const limit = 100;
+const answer = Math.floor(limit / 2);
