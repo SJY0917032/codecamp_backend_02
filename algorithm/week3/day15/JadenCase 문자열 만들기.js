405f59c07ab4fa78f4fb50ca639595b7829786fb
SJY0917032 <sea71511@gmail.com>
2022-04-03 22:43:34 +0900

    알고리즘_day15_리팩터링

--- algorithm/week3/day15/JadenCase 문자열 만들기.js
+++ algorithm/week3/day15/JadenCase 문자열 만들기.js
@@ -5,27 +5,39 @@
  *
  */
 function solution(s) {
-  let answer = "";
+  // let answer = "";
 
-  // 문자열의 첫글자를 대문자로 바꾸는 함수
-  function upper(s) {
-    return s.charAt(0).toUpperCase() + s.slice(1);
-  }
+  // // 문자열의 첫글자를 대문자로 바꾸는 함수
+  // function upper(s) {
+  //   return s.charAt(0).toUpperCase() + s.slice(1);
+  // }
 
-  // 들어온 단어를 공백기준으로 나눠준다
-  s = s.split(" ");
-  for (i = 0; i < s.length; i++) {
-    // 근데 공백이 2번이상 들어간 경우도있다
-    // 공백은 toLowerCase나 toUpperCase를 들어가면 Syntax에러를 만들기 때문에
-    // 조건문을 걸어준다.
-    if (s[i] !== " ") {
-      s[i] = s[i].toLowerCase();
-      s[i] = upper(s[i]);
-    }
-  }
+  // // 들어온 단어를 공백기준으로 나눠준다
+  // s = s.split(" ");
+  // for (i = 0; i < s.length; i++) {
+  //   // 근데 공백이 2번이상 들어간 경우도있다
+  //   // 공백은 toLowerCase나 toUpperCase를 들어가면 Syntax에러를 만들기 때문에
+  //   // 조건문을 걸어준다.
+  //   if (s[i] !== " ") {
+  //     s[i] = s[i].toLowerCase();
+  //     s[i] = upper(s[i]);
+  //   }
+  // }
 
-  // 합쳐준다
-  s = s.join(" ");
+  // // 합쳐준다
+  // s = s.join(" ");
 
-  return (answer = s);
+  // return (answer = s);
+
+  // 리팩터링
+
+  s = s
+    .toLowerCase()
+    .split(" ")
+    .map((str) => {
+      return str !== "" ? str[0].toUpperCase() + str.slice(1) : str;
+    })
+    .join(" ");
+
+  return s;
 }
