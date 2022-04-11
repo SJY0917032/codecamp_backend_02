b00038b3e0c4da2d231ee0d0fd0c5a8d52c6dc88
SJY0917032 <sea71511@gmail.com>
2022-04-11 12:30:21 +0900

    알고리즘_day21_시저암호(리팩터링)

--- algorithm/week5/day21/시저암호.js
+++ algorithm/week5/day21/시저암호.js
@@ -4,36 +4,56 @@
 "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
 */
 function solution(s, n) {
-  var answer = "";
-  // 들어온 문자열을 공백과 문자로 나눈다.
-  s = s.split("");
-  // 순회
-  for (i = 0; i < s.length; i++) {
-    // 공백판별
-    if (s[i] !== " ") {
-        // s[i]를 ascii코드로 바꾼걸 임시로 할당한다.
-      temp = s[i].charCodeAt();
-      // 소문자판별
-      if ("a" <= s[i] && "z" >= s[i]) {
-          // s[i] + n이 'z'을 넘어간경우 'a'로 밀어서 계산하기 위한 수식
-        if (temp + n > 122) {
-          s[i] = String.fromCharCode(96 + (temp + n - 122));
-        } else {
-          s[i] = String.fromCharCode(temp + n);
-        }
-      }
-      // 대문자판별 
-      else {
-          // 소문자와 동일하다.
-        if (temp + n > 90) {
-          s[i] = String.fromCharCode(64 + (temp + n - 90));
-        } else {
-          s[i] = String.fromCharCode(temp + n);
-        }
-      }
+  // var answer = "";
+  // // 들어온 문자열을 공백과 문자로 나눈다.
+  // s = s.split("");
+  // // 순회
+  // for (i = 0; i < s.length; i++) {
+  //   // 공백판별
+  //   if (s[i] !== " ") {
+  //       // s[i]를 ascii코드로 바꾼걸 임시로 할당한다.
+  //     temp = s[i].charCodeAt();
+  //     // 소문자판별
+  //     if ("a" <= s[i] && "z" >= s[i]) {
+  //         // s[i] + n이 'z'을 넘어간경우 'a'로 밀어서 계산하기 위한 수식
+  //       if (temp + n > 122) {
+  //         s[i] = String.fromCharCode(96 + (temp + n - 122));
+  //       } else {
+  //         s[i] = String.fromCharCode(temp + n);
+  //       }
+  //     }
+  //     // 대문자판별
+  //     else {
+  //         // 소문자와 동일하다.
+  //       if (temp + n > 90) {
+  //         s[i] = String.fromCharCode(64 + (temp + n - 90));
+  //       } else {
+  //         s[i] = String.fromCharCode(temp + n);
+  //       }
+  //     }
+  //   }
+  // }
+  // // n만큼 밀어낸 s[i]를 공백문자들과 합친다.
+  // answer = s.join("");
+  // return answer;
+
+  // 리팩터링
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const upper = lower.toUpperCase();
+
+  const answer = s.split("").reduce((acc, cur) => {
+    const word = lower.includes(cur) ? lower : upper;
+    let idx = word.indexOf(cur) + n;
+
+    if (idx >= 26) {
+      idx -= 26; // word의 범위를 벗어난것(다시 26을빼준값을 넣어준다)
     }
-  }
-  // n만큼 밀어낸 s[i]를 공백문자들과 합친다.
-  answer = s.join("");
+
+    return acc + (cur === " " ? " " : word[idx]);
+  }, "");
+
   return answer;
+
+  // reduce에는 문자열또한 포함될수있다
+  // return 값으로 공백이면 그냥 공백을 넘겨주고, 아니면 idx만큼 밀어준값을 return 시켜준다
 }
