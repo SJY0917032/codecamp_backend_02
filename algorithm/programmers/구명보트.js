304a2528ebbb478ade163f7e8a0f0d7aa48bfbac
SJY0917032 <sea71511@gmail.com>
2022-04-26 11:21:33 +0900

    프로그래머스_LV2-구명보트(효율성X)

---
+++ algorithm/programmers/구명보트.js
@@ -0,0 +1,28 @@
+function solution(people, limit) {
+  // 무게 순서대로 사람을 줄 세웁니다.
+  // 맨 앞에 있는 사람이랑 맨 뒤에 있는 사람이랑 보트에 담을 수 있으면 같이 보냅니다.
+  // 같이 못 보내면 맨 앞의 사람만 보트에 태워서 보내버립니다.
+  // 이걸 반복하다가, 맨 앞의 사람이 보트 제한 무게의 절반 이하가 되면,
+  // 무조건 맨 뒤의 사람과 같이 보낼 수 있으므로 남은 사람은 남은 사람 수의 절반의 보트만 있으면 됩니다.
+  people.sort((a, b) => b - a);
+  let answer = 0;
+  const half = limit / 2;
+  while (people[0] >= half) {
+    if (people[0] + people[people.length - 1] <= limit) {
+      people.shift();
+      people.pop();
+      answer++;
+    } else {
+      people.shift();
+      answer++;
+    }
+  }
+
+  if (people.length > 0) {
+    answer += Math.ceil(people.length / 2);
+  }
+  return answer;
+}
+
+// 푸는중 4.26...
+// 효율성 OUT
