0b44c47c277af08c2f59ab7b20b38bdbfd8293d1
SJY0917032 <sea71511@gmail.com>
2022-04-26 11:25:47 +0900

    구명보트 마무리

--- algorithm/programmers/구명보트.js
+++ algorithm/programmers/구명보트.js
@@ -1,28 +1,35 @@
 function solution(people, limit) {
-  // 무게 순서대로 사람을 줄 세웁니다.
-  // 맨 앞에 있는 사람이랑 맨 뒤에 있는 사람이랑 보트에 담을 수 있으면 같이 보냅니다.
-  // 같이 못 보내면 맨 앞의 사람만 보트에 태워서 보내버립니다.
-  // 이걸 반복하다가, 맨 앞의 사람이 보트 제한 무게의 절반 이하가 되면,
-  // 무조건 맨 뒤의 사람과 같이 보낼 수 있으므로 남은 사람은 남은 사람 수의 절반의 보트만 있으면 됩니다.
+  // 무개 순서대로 줄을세운다
   people.sort((a, b) => b - a);
   let answer = 0;
+  // 투포인터에서 맨앞사람이 만약 보트 무게의 절반이하가 된다면
   const half = limit / 2;
+
+  // 투포인터 할당
   let p1 = 0;
   let p2 = people.length - 1;
 
+  // 투포인터 반복문
   while (p1 <= p2) {
+    // 만약  pointer1이 무게절반이하가 된다면
     if (people[p1] <= half) {
+      // 남은 사람 수의 절반의 보트를 더해준다
       answer += Math.ceil((p2 + 1 - p1) / 2);
       break;
     }
+
+    // 만약 p1과 p2의사람의 무게가 limit 이하라면
     if (people[p1] + people[p2] <= limit) {
       p1++;
       p2--;
       answer++;
+      // 둘다 태워보낸다
     } else {
+      // 아니라면 한명만 보내버린다
       p1++;
       answer++;
     }
   }
+  // 최종적으로 필요했던 보트수를 반환
   return answer;
 }
