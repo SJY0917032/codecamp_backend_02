d37906c958b971bcdf9927401f9474b22f1f176c
SJY0917032 <sea71511@gmail.com>
2022-05-05 08:38:00 +0900

    강의_day38_쿠버네티스배포

--- class/33/33-01-deploy/backend/src/app.module.ts
+++ class/33/33-01-deploy/backend/src/app.module.ts
@@ -10,6 +10,8 @@ import { PointTransactionModule } from './apis/pointTransaction/pointTransaction
 import { ProductCategoryModule } from './apis/productCategory/productCategory.module';
 import { ProductModule } from './apis/products/products.module';
 import { UserModlue } from './apis/users/user.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -27,11 +29,11 @@ import { UserModlue } from './apis/users/user.module';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'class-db-0503', // 05-03  쿠버네티스에 배포된 db를입력한다. 하지만 따로 네트워크 배포를 해야만  서로 네임리졸루션이 되면서 연결됨.
+      host: '10.60.224.3', // 05-03  쿠버네티스에 배포된 db를입력한다. 하지만 따로 네트워크 배포를 해야만  서로 네임리졸루션이 되면서 연결됨.
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'mypod02',
+      database: 'myserver02',
       entities: [__dirname + '/apis/**'],
       synchronize: true,
       logging: true, // 로그를 남긴다 (명령어가 어떻게 바뀌는지)
@@ -39,8 +41,8 @@ import { UserModlue } from './apis/users/user.module';
       retryDelay: 5000,
     }),
   ],
-  // controllers: [AppController],
-  // providers: [AppService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
 
